6339ac1a1db65d6c7e90d225deb8f67163cb7bde
Dennis Koot <koter84@gmail.com>
2014-05-15 10:58:30 +0200

    coverity-scan working

---
+++ coverity_model.c
@@ -0,0 +1,9 @@
+/* Coverity Scan model
+ *
+ * This is a modeling file for Coverity Scan. Modeling helps to avoid false
+ * positives.
+ *
+ * Coverity Scan doesn't pick up modifications automatically. The model file
+ * must be uploaded by an admin in the analysis settings of
+ * http://scan.coverity.com/projects/2233
+ */
