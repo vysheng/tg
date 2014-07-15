3989599bfcc48c7f203d6808b630550156e5ec17
Rutrus <rutrus@gmail.com>
2014-07-16 00:04:15 +0200

    Solved queries.c conflict

--- queries.c
+++ queries.c
@@ -879,14 +879,9 @@ int msg_send_on_answer (struct query *q UU) {
       print_end ();
     }
   }
-<<<<<<< HEAD
-  if (verbosity)
-    rprintf ("Sent: id = %d\n", id);
-=======
   if (verbosity) {
 		rprintf ("Sent: id = %d\n", id);
 	}
->>>>>>> e75063e63d043e92c2fcdae5fe25491bd6a7df84
   bl_do_set_message_sent (M);
   return 0;
 }
@@ -960,14 +955,9 @@ void do_send_message (peer_id_t id, const char *msg, int len) {
   }
   long long t;
   secure_random (&t, 8);
-<<<<<<< HEAD
-  if (verbosity)
-    logprintf ("t = %lld, len = %d\n", t, len);
-=======
   if (verbosity) {
 		logprintf ("t = %lld, len = %d\n", t, len);
 	}
->>>>>>> e75063e63d043e92c2fcdae5fe25491bd6a7df84
   bl_do_send_message_text (t, our_id, get_peer_type (id), get_peer_id (id), time (0), len, msg);
   struct message *M = message_get (t);
   assert (M);
