0d2046a70fbc479b4122473fe7db68a740d83455
vvaltman <vvaltman@neon>
2014-08-31 14:04:16 +0400

    Added  changlog. increased version

--- tgl.h
+++ tgl.h
@@ -28,8 +28,8 @@
 #define TG_SERVER_TEST "173.240.5.253"
 
 // JUST RANDOM STRING
-#define TGL_BUILD "1828"
-#define TGL_VERSION "1.0-beta"
+#define TGL_BUILD "1934"
+#define TGL_VERSION "1.0.1"
 
 struct connection;
 struct mtproto_methods;
