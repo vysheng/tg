9a9f8d048d944801737f9ac936ba5f4c66855b5a
Vysheng <mail@vysheng.ru>
2014-09-08 17:33:33 +0400

    Fixed event-1.4

--- net.c
+++ net.c
@@ -477,6 +477,10 @@ static void try_read (struct connection *c) {
   if (!c->in_tail) {
     c->in_head = c->in_tail = new_connection_buffer (1 << 20);
   }
+  #ifdef EVENT_V1
+    struct timeval tv = {5, 0};
+    event_add (c->read_ev, &tv);
+  #endif
   int x = 0;
   while (1) {
     int r = read (c->fd, c->in_tail->wptr, c->in_tail->end - c->in_tail->wptr);
