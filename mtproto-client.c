34c7df2ae37433cf649f2f8093a8b627c85e68c8
Pablo Iranzo Gómez <Pablo.Iranzo@gmail.com>
2014-09-17 12:12:34 +0200

    Fix typo

--- mtproto-client.c
+++ mtproto-client.c
@@ -1280,7 +1280,7 @@ void tglmp_on_start (void) {
   }
 
   if (!ok) {
-    vlogprintf (E_ERROR, "No pubic keys found\n");
+    vlogprintf (E_ERROR, "No public keys found\n");
     exit (1);
   }
   
