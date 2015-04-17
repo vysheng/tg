0e1a619f02374d1e116a16b2168c27558b0822fa
Andre Martins <andmart@gmail.com>
2015-04-17 11:42:43 -0300

    Removed declarations in for loop to be C99 mode independent

--- util.c
+++ util.c
@@ -7,7 +7,7 @@ char* escape_char(char * str){
  	//Guessing
 	int chars_to_replace_position [100];
 	int chars_to_replace_position_idx = 0;
-
+	int i, j;
 	int start = 0;
 	int end = 0;
 	int pointer = 0;
@@ -17,7 +17,7 @@ char* escape_char(char * str){
 	char *buffer;
 
 
-	for(int i = 0; i < (int)strlen(str); i++){
+	for(i = 0; i < (int)strlen(str); i++){
 		if(str[i] == lookup[0] || str[i] == lookup[1]){
 			chars_to_replace_position[chars_to_replace_position_idx++] = i;
 		}
@@ -29,7 +29,7 @@ char* escape_char(char * str){
 		buffer = malloc(sizeof(char) * (strlen(str) + chars_to_replace_position_idx + 1));
 		
 
-		for (int j = 0; j < chars_to_replace_position_idx; j++){
+		for (j = 0; j < chars_to_replace_position_idx; j++){
 
 			end = chars_to_replace_position[j];
 			memcpy(buffer + pointer, str + start, (end - start));
