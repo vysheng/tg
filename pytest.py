30f0a3b1280a2a3f7017a442210d9d236cadc7fd
Vincent Castellano <surye80@gmail.com>
2015-05-03 22:42:11 -0700

    send_msg implemented

--- pytest.py
+++ pytest.py
@@ -16,9 +16,10 @@ def on_our_id(id):
     return "Set ID: " + str(our_id) 
 
 def on_msg_receive(msg):
-    pp.pprint(msg)    
+    if msg["out"]:
+      return;
+
     tgl.send_msg(msg["from"]["type"], msg["from"]["id"], "PONG!")
-    return "Got msg from " + msg["from"]["peer"]["first_name"]
 
 def on_secret_chat_update(peer, types):
     return "on_secret_chat_update"
