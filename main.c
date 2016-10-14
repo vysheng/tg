44305412246186bae3990d404b31e66af1433582
Kimiyuki Onaka <kimiyuki95@gmail.com>
2016-10-14 17:06:40 +0900

    Fix typo of option in usage

--- main.c
+++ main.c
@@ -488,7 +488,7 @@ void usage (void) {
   printf ("  --groupname/-G <group-name>          change gid after start\n");
   printf ("  --disable-output/-D                  disable output\n");
   printf ("  --tcp-port/-P <port>                 port to listen for input commands\n");
-  printf ("  --udp-socket/-S <socket-name>        unix socket to create\n");
+  printf ("  --unix-socket/-S <socket-name>       unix socket to create\n");
   printf ("  --exec/-e <command>                  make a single command end then exit safely\n");
   printf ("  --disable-names/-I                   use user and chat IDs in updates instead of names\n");
   printf ("  --enable-ipv6/-6                     use ipv6 (may be unstable)\n");
